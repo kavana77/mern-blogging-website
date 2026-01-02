47e8050ee2c8129ab452c8fc358e5bd9753b4f6d
kavana77 <kavanab77@gmail.com>
2026-01-02 19:03:57 +0530

    add / in url

--- server/src/server.ts
+++ server/src/server.ts
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "https://mern-blogging-website-scyf.vercel.app",
+    origin: "https://mern-blogging-website-scyf.vercel.app/",
     credentials: true,
   })
 );
