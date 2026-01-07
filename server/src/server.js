071cca33dcdfe5555c306984fe6d98832045ec66
kavana77 <kavanab77@gmail.com>
2026-01-07 12:50:25 +0530

    deploy ready

--- server/src/server.js
+++ server/src/server.js
@@ -16,7 +16,7 @@ const fs_1 = __importDefault(require("fs"));
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://mern-blogging-website-three.vercel.app"],
     credentials: true,
 }));
 app.use(express_1.default.json());
